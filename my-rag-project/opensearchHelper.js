3333789f5089c78368ee70d6195d9ef8b2da54f9
Toshifumi Kurosawa <kuromailserver@gmail.com>
2025-03-10 15:28:08 +0900

    Add authentication to OpenSearch client configuration
    This commit updates the OpenSearch client setup in opensearchHelper.js to include authentication credentials, improving the connection security and allowing direct authentication with the OpenSearch endpoint.

--- my-rag-project/opensearchHelper.js
+++ my-rag-project/opensearchHelper.js
@@ -5,12 +5,13 @@ const { Client } = require("@opensearch-project/opensearch");
  * OpenSearch クライアントのセットアップ
  *  - endpoint: 自分のドメインのエンドポイントを設定
  */
-const osClient = new Client({
-  node: "https://search-abc-company-3-viafjf6tndjbkgztqtwadgyiai.ap-northeast-1.es.amazonaws.com", 
-  // ↑ OpenSearchのエンドポイントURLを指定
-  // AWS SigV4認証を使う場合は別途@opensearch-project/opensearch/aws 
-  // を使った署名プラグインなどが必要になる場合があります。
-});
+const client = new Client({
+    node: "https://search-abc-company-3-viafjf6tndjbkgztqtwadgyiai.ap-northeast-1.es.amazonaws.com",
+    auth: {
+      username: "abc-company-user",
+      password: "abc-company-Pw1!"
+    }
+  });
 
 /**
  * インデックスに文書を登録 (k-NN用のベクトルフィールドを含む)
